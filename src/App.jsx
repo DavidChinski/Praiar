5edb67dc268eda54d24edf62662310f390203e4a
DAVID CHINSKI <48592365@ort>
2025-05-09 12:01:28 -0300

    Agregar logos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Paiar</h1>
+      <h2>Bro Bebzcuk piensa que va a convencer a hebraica de usar su proyecto</h2>
+    </>
+  )
+}
+
+export default App
