637e1bf7bf72a1004a50fe3de74aa4f42e7cc511
Android Studio <devandroid@ort>
2025-06-09 09:00:20 -0300

    banner de tu balneario quitado

--- src/Pages/TusBalnearios.jsx
+++ src/Pages/TusBalnearios.jsx
@@ -1,12 +1,11 @@
 import Layout from '../Layout/'
 import React from "react";
 import BalneariosComponent from '../components/BalneariosComponent/'
-import BanerTusBalnearios from '../components/BanerTusBalnearios/'
 import CrearBalneario from '../Components/CrearBalneario';
 function TusBalnearios() {
   return (
       <Layout >
-        <BanerTusBalnearios />
+  
         <div id="seccion-inferior">
         {/* contenido que viene después */}
           <BalneariosComponent />
